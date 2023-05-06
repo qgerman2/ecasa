1bb3f86d72061c526a43dfd02ea18deb55a73d2f
qgerman2 <qgerman2@gmail.com>
2023-05-05 20:57:21 -0400

    graficos matlab

--- graficos.m
+++ graficos.m
@@ -1,2 +1,72 @@
 [t, x, y, z, vx, vy, vz, wx, wy, wz, q0, q1, q2, q3] = importar();
-plot(t, z)
+
+f1 = figure(1);
+title("Posición")
+f1.Position(3:4) = [500, 200];
+hold on
+plot(t, z, "DisplayName", "z", LineWidth=1)
+plot(t, y, "DisplayName", "y", LineWidth=1)
+plot(t, x, "DisplayName", "x", LineWidth=1)
+grid on
+xlabel("Tiempo [s]", "Interpreter","latex")
+ylabel("Distancia [m]", "Interpreter","latex")
+legend("location", "southwest", "Interpreter","latex")
+
+f2 = figure(2);
+title("Velocidad")
+f2.Position(3:4) = [500, 200];
+hold on
+plot(t, vz, "DisplayName", "$v_z$", LineWidth=1)
+plot(t, vy, "DisplayName", "$v_y$", LineWidth=1)
+plot(t, vx, "DisplayName", "$v_x$", LineWidth=1)
+grid on
+xlabel("Tiempo [s]", "Interpreter","latex")
+ylabel("Velocidad [m/s]", "Interpreter","latex")
+legend("location", "northeast", "Interpreter","latex")
+
+f3 = figure(3);
+title("Velocidad angular Body-frame")
+f3.Position(3:4) = [500, 200];
+hold on
+plot(t, wz, "DisplayName", "$\omega_z$", LineWidth=1)
+plot(t, wy, "DisplayName", "$\omega_y$", LineWidth=1)
+plot(t, wx, "DisplayName", "$\omega_x$", LineWidth=1)
+grid on
+xlabel("Tiempo [s]", "Interpreter","latex")
+ylabel("Velocidad angular [rad/s]", "Interpreter","latex")
+ylim([-0.1, 0.1])
+legend("location", "northeast", "Interpreter","latex")
+
+f4 = figure(4);
+title("Velocidad angular Inertial-frame")
+f4.Position(3:4) = [500, 200];
+hold on
+plot(t, q0, "DisplayName", "$q_0$", LineWidth=1)
+plot(t, q1, "DisplayName", "$q_1$", LineWidth=1)
+plot(t, q2, "DisplayName", "$q_2$", LineWidth=1)
+plot(t, q3, "DisplayName", "$q_3$", LineWidth=1)
+ylim([-1.1 1.1])
+grid on
+xlabel("Tiempo [s]", "Interpreter","latex")
+ylabel("Valor del componente", "Interpreter","latex")
+legend("location", "northeast", "Interpreter","latex")
+
+angle = quat2eul([q0, q1, q2, q3]);
+f5 = figure(5);
+title("Velocidad angular Inertial-frame")
+f5.Position(3:4) = [500, 200];
+hold on
+plot(t, angle(:,1), "DisplayName", "$yaw \phi$", LineWidth=1)
+plot(t, angle(:,2), "DisplayName", "$pitch \theta$", LineWidth=1)
+plot(t, angle(:,3), "DisplayName", "$roll \psi$", LineWidth=1)
+ylim([-pi-0.1, pi+0.1])
+grid on
+xlabel("Tiempo [s]", "Interpreter","latex")
+ylabel("Angulo [rad]", "Interpreter","latex")
+legend("location", "northeast", "Interpreter","latex")
+
+exportgraphics(f1, "1pos.emf")
+exportgraphics(f2, "2vel.emf")
+exportgraphics(f3, "3velangbod.emf")
+exportgraphics(f4, "4velangine.emf")
+exportgraphics(f5, "5velanginecuat.emf")
